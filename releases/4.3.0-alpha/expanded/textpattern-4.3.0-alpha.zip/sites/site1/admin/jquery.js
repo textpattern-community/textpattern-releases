6054e86f59b74b20f8fdd7ee329480364e0655e5
Pete Cooper <pete@pete-mbp17.local>
2015-01-23 12:34:04 +0000

    Add Textpattern 4.3.0

---
+++ releases/4.3.0-alpha/expanded/textpattern-4.3.0-alpha.zip/sites/site1/admin/jquery.js
@@ -0,0 +1,1 @@
+link ../../../textpattern/jquery.js
---
+++ releases/4.3.0-alpha/expanded/textpattern-4.3.0-alpha.zip/textpattern/css.php
@@ -0,0 +1,16 @@
+<?php
+/*
+@deprecated
+@see ../css.php
+
+$HeadURL: http://textpattern.googlecode.com/svn/development/4.x/textpattern/css.php $
+$LastChangedRevision: 3189 $
+*/
+
+if (!defined("txpath"))
+{
+	define("txpath", dirname(__FILE__));
+}
+
+require_once txpath.'/../css.php';
+?>
