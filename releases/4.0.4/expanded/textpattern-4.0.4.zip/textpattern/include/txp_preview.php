aeffa15daea285a5cad155bf2901ce7257f78915
Pete Cooper <pete@pete-mbp17.local>
2015-01-23 13:13:58 +0000

    Add Textpattern 4.0.4

---
+++ releases/4.0.4/expanded/textpattern-4.0.4.zip/textpattern/include/txp_preview.php
@@ -0,0 +1,12 @@
+<?php
+/*
+$HeadURL: http://svn.textpattern.com/releases/4.0.4/source/textpattern/include/txp_preview.php $
+$LastChangedRevision: 1238 $
+*/
+if (!defined('txpinterface')) die('txpinterface is undefined.');
+
+include 'publish.php';
+
+textpattern();
+
+?>
