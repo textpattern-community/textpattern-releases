b559029c92007fdf8f470120bf2daed623a41f94
Pete Cooper <pete@pete-mbp17.local>
2015-01-23 12:21:05 +0000

    Add Textpattern 4.5.0

---
+++ releases/4.5.0-beta/expanded/textpattern-4.5.0-beta.tar.gz/textpattern/update/_to_4.0.3.php
@@ -0,0 +1,11 @@
+<?php
+/*
+$HeadURL: http://textpattern.googlecode.com/svn/development/4.x/textpattern/update/_to_4.0.3.php $
+$LastChangedRevision: 4011 $
+*/
+	if (!defined('TXP_UPDATE'))
+		exit("Nothing here. You can't access this file directly.");
+
+	safe_update('txp_form',"Form = CONCAT('<txp:comments_error wraptag=\"ul\" break=\"li\" />\n\n',Form)", "name LIKE 'comment_form'");
+
+?>
---
+++ releases/4.5.0-beta/expanded/textpattern-4.5.0-beta.zip/textpattern/update/_to_4.4.0.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+$HeadURL: http://textpattern.googlecode.com/svn/development/4.x/textpattern/update/_to_4.4.0.php $
+$LastChangedRevision: 4011 $
+*/
+
+	if (!defined('TXP_UPDATE'))
+	{
+		exit("Nothing here. You can't access this file directly.");
+	}
+
+//	This page intentionally left blank.
+
+?>
